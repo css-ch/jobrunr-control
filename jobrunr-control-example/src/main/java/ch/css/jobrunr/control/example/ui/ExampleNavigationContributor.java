5b2f0d0337ff2ece2309174efe6c639be4275026
Marc Schuler <mschuler@bluewin.ch>
2026-08-28 14:08:46 +0200

    Example für Navigation und Branding. quarkus.profile=ui-extensions

---
+++ jobrunr-control-example/src/main/java/ch/css/jobrunr/control/example/ui/ExampleNavigationContributor.java
@@ -0,0 +1,27 @@
+package ch.css.jobrunr.control.example.ui;
+
+import ch.css.jobrunr.control.adapter.ui.shell.JobRunrControlNavigationContributor;
+import ch.css.jobrunr.control.adapter.ui.shell.JobRunrControlNavigationItem;
+import io.quarkus.arc.profile.IfBuildProfile;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Example host-application navigation enabled with the {@code ui-extensions} profile.
+ */
+@ApplicationScoped
+@IfBuildProfile("ui-extensions")
+public class ExampleNavigationContributor implements JobRunrControlNavigationContributor {
+
+    @Override
+    public List<JobRunrControlNavigationItem> navigationItems() {
+        return List.of(new JobRunrControlNavigationItem(
+                "google-search",
+                "Google-Suche",
+                "https://www.google.com/search?q=JobRunr+Pro",
+                "search",
+                Set.of()));
+    }
+}
